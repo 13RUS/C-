e9368ca966bb83fc30495d2ea72fee3bb1bcf488
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-04-09 14:46:51 +0300

    Add files via upload

---
+++ otus-cpp-basics/01.hello_world/move_me_out.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Aleksey Leshchankin on 09.04.2022.
+//
+
+#include "move_me_out.h"
+#include <iostream>
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
+
---
+++ otus-cpp-basics/01.hello_world/move_me_out.h
@@ -0,0 +1,6 @@
+//
+// Created by Aleksey Leshchankin on 09.04.2022.
+//
+#pragma once
+
+void todo_move_me_out();
