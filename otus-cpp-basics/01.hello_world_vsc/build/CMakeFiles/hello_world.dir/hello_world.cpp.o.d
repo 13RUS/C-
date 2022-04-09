70fead560a69ccfb1d8e522afddfe6c1bca5bf39
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-04-09 14:48:05 +0300

    Add files via upload

---
+++ otus-cpp-basics/01.hello_world_vsc/build/CMakeFiles/hello_world.dir/hello_world.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/hello_world.dir/hello_world.cpp.o: \
+  /Users/alekseyleshchankin/Library/Mobile\ Documents/com~apple~CloudDocs/Code\ iCloud/otus-cpp-basics/01.hello_world_vsc/hello_world.cpp
