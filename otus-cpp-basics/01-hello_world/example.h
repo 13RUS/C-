2e86f5114fe0b0fea6cba160ea9bcd33b5a58bd6
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-04-09 14:46:12 +0300

    Delete otus-cpp-basics/01-hello_world directory

--- otus-cpp-basics/01-hello_world/example.h
+++
@@ -1,6 +0,0 @@
-//
-// Created by Aleksey Leshchankin on 09.04.2022.
-//
-#pragma once
-
-void example_function();
--- otus-cpp-basics/01-hello_world/move_me_out.h
+++
@@ -1,6 +0,0 @@
-//
-// Created by Aleksey Leshchankin on 09.04.2022.
-//
-#pragma once
-
-void todo_move_me_out();
