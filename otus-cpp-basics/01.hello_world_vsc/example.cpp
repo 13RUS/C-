9d32a75d72ee81cbe63a8e4d38649b923f44b0d0
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-04-09 14:44:31 +0300

    Add files via upload

---
+++ otus-cpp-basics/01.hello_world_vsc/example.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Aleksey Leshchankin on 09.04.2022.
+//
+
+#include "example.h"
+#include <iostream>
+
+void example_function() {
+    std::cout << "Hello from example function!" << std::endl;
+}
+
