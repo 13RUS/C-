5970499cfc5b61d443444788991350cd3a6229e6
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-07-27 01:15:41 +0400

    Add files via upload

--- otus-cpp-basics/07.gtest/src/main.cpp
+++ otus-cpp-basics/07.gtest/src/main.cpp
@@ -8,38 +8,15 @@ int main(int, char**) {
     std::cout << "Vector Testing" << std::endl;
 
     homeworkspace::Vector<size_t> vector;
-    size_t count_test = 10;
+    size_t count = 10;
 
-    for (size_t i = 0; i < count_test; ++i) {
+    for (size_t i = 0; i < count; ++i) {
         vector.push_back(i);
     }
 
-    vector.pop_middle();
-
-    for (size_t i = 0; i < count_test-1 ; ++i) {
-        std::cout << vector.front() << ' ';
-        vector.pop_front();
+    for (size_t i = 0; i < count; ++i) {
+        std::cout << vector[i] << ' ';
     }
 
-    //std::cout << "\n vector size = " << vector.size();
-    //std::cout << "\n vector size typedef = " << typeid(int(vector.size())).name();
-
-    /*
-     size_t count_test = 10;
-     homeworkspace::List<size_t> list;
-
-     for (size_t i = 0; i < count_test; ++i) {
-        list.push_back(i);}
-
-       for (size_t i = 0; i < count; ++i) {
-           list.push_back(i);
-           list.push_front(i);
-       }
-
-       for (size_t i = 0; i < count * 2; ++i) {
-           std::cout << list.front() << ' ';
-           list.pop_front();
-       }
-    */
      return 0;
 }
