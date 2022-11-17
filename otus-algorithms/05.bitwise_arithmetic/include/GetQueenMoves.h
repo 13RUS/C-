94356bab0817809a10b810be78b59eebb592212e
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-11-17 14:39:50 +0300

    Add files via upload

--- otus-algorithms/05.bitwise_arithmetic/include/GetQueenMoves.h
+++ otus-algorithms/05.bitwise_arithmetic/include/GetQueenMoves.h
@@ -10,29 +10,6 @@
 // ---------------------------------------
 unsigned long GetQueenMoves (unsigned int position) {
     unsigned long bitmask = 0;
-    unsigned long bitmasktmp;
-    unsigned long K = 1UL << position;
-    unsigned int y = 0;
-
-    // -- Decteting y-coordinate --
-    for (int i = 1; i < 9; i ++){
-        if (position <= (i*8-1)) {
-            y++;
-            break;
-        }
-        y++;
-    }
-
-    //std::cout <<"position =  " << K << std::endl;
-    bitmasktmp = GetRookMoves(position);
-   // std::cout <<"Rook Mask  =  " << bitmasktmp << std::endl;
-
-    bitmask = bitmask | bitmasktmp;
-    bitmasktmp = GetBishopMoves(position);
-   // std::cout <<"Bishop Mask  =  " << bitmasktmp << std::endl;
-    bitmask = bitmask | bitmasktmp;
-
-  // std::cout <<"Queen Mask  =  " << bitmask << std::endl;
-
+    bitmask = bitmask | GetRookMoves(position) | GetBishopMoves(position);;
     return bitmask;
 }
