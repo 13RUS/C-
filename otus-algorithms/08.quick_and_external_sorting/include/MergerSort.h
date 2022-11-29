cb4082932468eee84429150c0415b8311432406c
Aleksei Leshchankin <a.leshchankin.evol@gmail.com>
2022-11-29 15:18:19 +0300

    Add files via upload

---
+++ otus-algorithms/08.quick_and_external_sorting/include/MergerSort.h
@@ -0,0 +1,10 @@
+//
+// Created by Aleksey Leshchankin on 25.11.2022.
+//
+
+#pragma once
+
+void merge (int array[], int p, int q, int r);
+void merge (int *array, int p, int q, int r, unsigned long& cmp, unsigned long& asg);
+
+void MergeSort (int* array, int left, int right, unsigned long& cmp, unsigned long& asg, int& time);
---
+++ otus-algorithms/08.quick_and_external_sorting/include/QuickSort.h
@@ -0,0 +1,8 @@
+//
+// Created by Aleksey Leshchankin on 24.11.2022.
+//
+
+#pragma once
+
+void QuickSort (int* array, int low, int high, unsigned long& cmp, unsigned long& asg, int& time);
+int partition (int* array, int low, int high, unsigned long& cmp, unsigned long& asg);
